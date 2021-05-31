f137896f824ffa0230e0234be2fae3b9410eaba4
in <in@verocity.jp>
2021-06-01 01:46:11 +0900

    音声コマンド実装

--- src/bot.js
+++ src/bot.js
@@ -4,9 +4,9 @@ const env = process.env
 const Discord = require('discord.js');
 const speech = require('@google-cloud/speech');
 const speechClient = new speech.SpeechClient()
-
-
 const client = new Discord.Client();
+
+const command = config.bot.command
 
 client.once('ready', async () => {
   const allCh = client.channels.cache.array()
@@ -16,13 +16,18 @@ client.once('ready', async () => {
 
   const voiceConn = await voiceCh.join()
   console.log('join')
+  
+  let lastTalk
+  let volume = 80
+  const volumeRate = 1000
+  let musicDispatcher
 
-  // const musicDispatcher = voiceConn.play('sample.mp3')
-  // musicDispatcher.setVolume(40 / 1000)
+  greetingDispatcher = voiceConn.play('resource/reply.mp3')
+  greetingDispatcher.setVolume(0)
 
-  voiceConn.once('speaking', async (user, speaking) => {
-    const receiver = voiceConn.receiver
+  const receiver = voiceConn.receiver
 
+  voiceConn.on('speaking', async (user, speaking) => {
     if(!speaking) return
 
     const audioStream = receiver.createStream(user, { mode: 'pcm' });
@@ -40,8 +45,63 @@ client.once('ready', async () => {
     const recognizeStream = speechClient
       .streamingRecognize(request)
       .on('error', console.error)
-      .on('data', data => {
-        const result = data.results[0].alternatives[0].transcript
+      .on('data', async data => {
+        let result = data.results[0].alternatives[0].transcript
+
+        regexp = new RegExp('^' + command.prefix + '(.+$)')
+        const prefixExists = result.match(regexp)
+        console.log(prefixExists)
+        if (lastTalk === command.prefix || prefixExists) {
+
+          if (prefixExists) {
+            result = result.replace(regexp, "$1")
+          }
+          console.log('command start')
+
+          switch (result) {
+            case command.sampleMusic:
+              const replyDispatcher = voiceConn.play('resource/reply.mp3')
+              replyDispatcher.setVolume(200 / volumeRate)
+    
+              replyDispatcher.on('finish', () => {
+                console.log('sample music start')
+                setTimeout(() => {
+                  musicDispatcher = voiceConn.play('resource/sample-music.mp3')
+                  musicDispatcher.setVolume(volume / volumeRate)  
+
+                  musicDispatcher.on('finish', async () => {
+                    setTimeout(() => {
+                      voiceConn.disconnect()
+                    }, 3000);
+                  })
+                }, 2000);
+              })
+              break;
+
+            case command.volumeUp:
+              if (!musicDispatcher) return
+              console.log('volume up')
+              volume += 20
+              musicDispatcher.setVolume(volume / volumeRate)  
+              break;
+
+            case command.volumeDown:
+              if (!musicDispatcher) return
+              console.log('volume down')
+              volume -= 20
+              if (volume < 0) {
+                volume = 0
+              }
+              musicDispatcher.setVolume(volume / volumeRate)  
+              break;
+          
+            default:
+              break;
+          }
+
+        }
+
+        lastTalk = result
         // textCh.send(result)
         console.log(result);
       });
